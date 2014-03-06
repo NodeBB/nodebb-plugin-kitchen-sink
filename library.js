bfb09d5f837f747cc5e84b2dd925a465fe5129a8
psychobunny <psycho.bunny@hotmail.com>
2014-03-06 16:50:43 -0500

    static page + static admin page examples

--- library.js
+++ library.js
@@ -3,6 +3,56 @@
 
 	var Plugin = {};
 
+	function renderSink(req, res, next) {
+		var data = { test: [{"name": "psychobunny"},{"name": "barisusakli"},{"name": "julianlam"}] };
+
+		if (res.locals.isAPI) {
+			res.json(data);
+		} else {
+			res.render('demo', data)
+		}
+	}
+
+	function renderAdmin(req, res, next) {
+		if (res.locals.isAPI) {
+			res.json({});
+		} else {
+			res.render('admin/kitchen-sink', {});
+		}
+	}
+
+	Plugin.init = function(app, middleware, controllers) {
+		app.get('/demo', middleware.buildHeader, renderSink);
+		app.get('/api/demo', renderSink);
+
+		app.get('/admin/kitchen-sink', middleware.admin.buildHeader, renderAdmin);
+		app.get('/api/admin/kitchen-sink', renderAdmin);
+	};
+
+	Plugin.addNavigation = function(header, callback) {
+		header.navigation = header.navigation.concat(
+			[
+				{
+					route: '/demo',
+					class: '',
+					text: "Kitchen Sink"
+				}
+			]
+		);
+
+		return header;
+	};
+
+	Plugin.addAdminNavigation = function(header, callback) {
+		header.plugins.push({
+			route: '/kitchen-sink',
+			icon: 'fa-tint',
+			name: 'Kitchen Sink'
+		});
+
+		return header;
+	};
+
 	Plugin.addUserSettings = function(settings, callback) {
 		// todo: use settings.tpl instead
 		settings.push({
---
+++ templates/demo.tpl
@@ -0,0 +1,13 @@
+<h1>Here is my Kitchen Sink Demo</h1>
+
+<span>works perfectably.</span>
+<hr />
+
+Developers of NodeBB
+<ul>
+	<!-- BEGIN test -->
+	<li>
+		{test.name}
+	</li>
+	<!-- END test -->
+</ul>
