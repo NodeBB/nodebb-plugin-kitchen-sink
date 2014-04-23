fd4adf96d57d66b15f3debcbf761fc5e3158f77e
psychobunny <psycho.bunny@hotmail.com>
2014-04-23 19:34:43 -0400

    reorganized kitchen sink

--- library.js
+++ library.js
@@ -1,81 +1,18 @@
 (function(module) {
 	"use strict";
 
-	var Plugin = {},
-		db = module.parent.require('../src/database');
-
-	function renderSink(req, res, next) {
-		var data = { test: [{"name": "psychobunny"},{"name": "barisusakli"},{"name": "julianlam"}] };
-
-		res.render('demo', data);
-	}
-
-	function renderAdmin(req, res, next) {
-		res.render('admin/kitchen-sink', {});
-	}
+	var Plugin = {};
+	
 
 	Plugin.init = function(app, middleware, controllers) {
-		app.get('/demo', middleware.buildHeader, renderSink);
-		app.get('/api/demo', renderSink);
-
-		app.get('/admin/kitchen-sink', middleware.admin.buildHeader, renderAdmin);
-		app.get('/api/admin/kitchen-sink', renderAdmin);
+		require('./lib/customRoutes')(app, middleware, controllers);
+		require('./lib/adminPage')(app, middleware, controllers);
 	};
 
-	Plugin.addNavigation = function(header, callback) {
-		header.navigation = header.navigation.concat(
-			[
-				{
-					route: '/demo',
-					class: '',
-					text: "Kitchen Sink"
-				}
-			]
-		);
 
-		callback(null, header);
-	};
-
-	Plugin.addAdminNavigation = function(header, callback) {
-		header.plugins.push({
-			route: '/kitchen-sink',
-			icon: 'fa-tint',
-			name: 'Kitchen Sink'
-		});
-
-		callback(null, header);
-	};
-
-	Plugin.addUserSettings = function(settings, callback) {
-		// todo: use settings.tpl instead
-		settings.push({
-			title: "Kitchen Sink",
-			content: "<label>Forum Title</label><input type='text' data-property='userTitle' placeholder='Enter Title' class='form-control' />"
-		});
-
-		callback(null, settings);
-	};
-	
-	Plugin.getUserSettings = function(data, callback) {
-		db.getObjectField('user:' + data.uid + ':settings', 'userTitle', function(err, title) {
-			if (err) {
-				callback(err);
-			}	
-			data.settings.userTitle = title;
-			callback(null, data);
-		});	
-	};
-	
-	Plugin.saveUserSettings = function(data) {
-		if (data.uid && data.userTitle) {
-			db.setObjectField('user:' + data.uid + ':settings', 'userTitle', data.userTitle);
-		}
-	};
-
-	Plugin.addAdminScripts = function(scripts, callback) {
-		scripts.push('/plugins/nodebb-plugin-kitchen-sink/lib/admin.js');
-		callback(null, scripts);
-	};
-
+	require('./lib/nodebb');
+	require('./lib/userSettings')(Plugin);
+	require('./lib/menuItems')(Plugin);
+	require('./lib/clientScripts')(Plugin);
 	module.exports = Plugin;
 }(module));
