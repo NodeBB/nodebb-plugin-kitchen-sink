98aa2b42b9f1a3a1294b23dbb359cfcdd32ea691
psychobunny <psycho.bunny@hotmail.com>
2014-11-11 18:00:01 -0500

    fixing router issue

--- library.js
+++ library.js
@@ -46,7 +46,7 @@
 
 	// static:app.load
 	Plugin.init = function(params, callback) {
-		var app = params.app,
+		var app = params.router,
 			middleware = params.middleware,
 			controllers = params.controllers;
 			
